045b1e10f41b10d8d9e2e03d6c0a66b0a73469fe
谢石长 <xieshichang@xieshichangdeMacBook-Air.local>
2017-03-22 22:04:30 +0800

    启动图片和目录划分

---
+++ 🐟斗鱼直播/🐟斗鱼直播Tests/______Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ______Tests.swift
+//  🐟斗鱼直播Tests
+//
+//  Created by 谢石长 on 17/3/22.
+//  Copyright © 2017年 谢石长. All rights reserved.
+//
+
+import XCTest
+@testable import ______
+
+class ______Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
