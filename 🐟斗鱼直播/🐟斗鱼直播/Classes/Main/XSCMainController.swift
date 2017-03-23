e7828b360ca7bafd9de29105b2dd4865cf8b196c
谢石长 <xieshichang@xieshichangdemacbook-air.local>
2017-03-23 10:40:31 +0800

    代码在tabbarControl里从storyboard添加控制器

--- 🐟斗鱼直播/🐟斗鱼直播/Classes/Main/XSCMainController.swift
+++ 🐟斗鱼直播/🐟斗鱼直播/Classes/Main/XSCMainController.swift
@@ -12,24 +12,32 @@ class XSCMainController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+//        let vc=UIViewController()
+//        vc.view.backgroundColor=UIColor.green
+//
+//        addChildViewController(vc)
+        
+        // 1.通过storyboard获取控制器;
+        //let childVc=UIStoryboard.init(name:"Home", bundle: nil).instantiateInitialViewController()
+        // 2 将childVc作为子控制器
+       // addChildViewController(childVc!)  // ! 用于将instantiateInitialViewController()－［可选类型］－转换为确定类型
+        
+//        addChildVc(storyName: "Home")
+//        addChildVc(storyName: "Live")
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func addChildVc(storyName:String){
+         // 1.通过storyboard获取控制器;
+        let childVc=UIStoryboard.init(name: storyName, bundle: nil).instantiateInitialViewController()!
+        
+        // 2 将childVc作为子控制器
+        addChildViewController(childVc)
+        
+        
+        
     }
-    */
 
+ 
 }
